164735802bf34ba6fc42c10411599f29853c4c06
Jean Macedo <jfmacedo91@gmail.com>
2021-07-23 00:15:05 -0300

    :wrench: chakra ui config

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#1A161F",
+      "800": "#221D26",
+      "700": "#383040",
+      "600": "#594D66",
+      "500": "#706080",
+      "400": "#8A7A99",
+      "300": "#A598B3",
+      "200": "#C2B8CC",
+      "100": "#D3CED9",
+      "50": "#F0EDF2"
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50'
+      }
+    }
+  }
+})
