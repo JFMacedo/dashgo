ec55bc3fb210f83ea5ad576d4f2f3fdd0809c492
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-08-01 08:52:07 -0300

    :straight_ruler: responsive user list

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,10 +1,11 @@
-import { Box, HStack } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
 
 import { PaginationItem } from "./PaginationItem"
 
 export function Pagination() {
   return (
-    <HStack
+    <Stack
+      direction={["column", "row"]}
       spacing="6"
       marginTop="8"
       alignItems="center"
@@ -13,12 +14,12 @@ export function Pagination() {
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
-      <HStack spacing="2">
+      <Stack direction="row" spacing="2">
         <PaginationItem isCurrent number={ 1 } />
         <PaginationItem number={ 2 } />
         <PaginationItem number={ 3 } />
         <PaginationItem number={ 4 } />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,203 +11,143 @@ import {
   Text,
   Th,
   Thead,
-  Tr
+  Tr,
+  useBreakpointValue
 } from "@chakra-ui/react"
 
-import {
-  FaPencilAlt,
-  FaPlusCircle
-} from "react-icons/fa"
+import { FaPencilAlt, FaPlusCircle } from "react-icons/fa"
 
-import {
-  Header
-} from "../../components/Header"
+import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
-
-import {
-  Sidebar
-} from "../../components/Sidebar"
+import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
       <Flex
-        width='100%'
-        maxWidth='1480px'
-        marginY='6'
-        marginX='auto'
-        paddingX='6'
+        width="100%"
+        maxWidth="1480px"
+        marginY="6"
+        marginX="auto"
+        paddingX="6"
       >
         <Sidebar />
         <Box
-          flex='1'
-          padding='8'
-          borderRadius='8px'
-          backgroundColor='gray.800'
+          flex="1"
+          padding={["4", "8"]}
+          borderRadius="8px"
+          backgroundColor="gray.800"
           boxShadow="md"
         >
           <Flex
-            alignItems='center'
-            justifyContent='space-between'
-            marginBottom='8'
+            alignItems="center"
+            justifyContent="space-between"
+            marginBottom="8"
           >
-            <Heading
-              size='lg'
-              fontWeight='400'
-            >
+            <Heading size="lg" fontWeight="400">
               Usuários
             </Heading>
             <Button
-              as='a'
-              size='sm'
-              fontSize='sm'
-              colorScheme='green'
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="green"
               leftIcon={ <Icon as={ FaPlusCircle } /> }
             >
               Criar novo
             </Button>
           </Flex>
-          <Table
-            colorScheme='whiteAlpha'
-          >
+          <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th
-                  width='8'
-                  paddingX='6'
-                  color='gray.300'
-                >
-                  <Checkbox
-                    colorScheme='green'
-                  />
+                <Th width="8" paddingX={ ["4", "4", "6"] } color="gray.300">
+                  <Checkbox colorScheme="green" />
                 </Th>
-                <Th>
-                  Usuário
-                </Th>
-                <Th>
-                  Data de cadastro
-                </Th>
-                <Th
-                  width='8'
-                >
-                  Editar
-                </Th>
+                <Th>Usuário</Th>
+                { isWideVersion && <Th>Data de cadastro</Th> }
+                { isWideVersion && <Th width="8">Editar</Th> }
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td
-                  padding='6'
-                >
-                  <Checkbox
-                    colorScheme='green'
-                  />
+                <Td  paddingX={ ["4", "4", "6"] }>
+                  <Checkbox colorScheme="green" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text
-                      fontWeight='bold'
-                    >
-                      Jéssica Lais Cuaglio
-                    </Text>
-                    <Text
-                      fontSize='sm'
-                      color='gray.300'
-                    >
+                    <Text fontWeight="bold">Jéssica Lais Cuaglio</Text>
+                    <Text fontSize="sm" color="gray.300">
                       jessica_lcuaglio@hotmail.com
                     </Text>
                   </Box>
                 </Td>
+                { isWideVersion && <Td>13 de Maio, 2021</Td> }
                 <Td>
-                  13 de Maio, 2021
-                </Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
+                  { isWideVersion && (<Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
                     leftIcon={ <Icon as={ FaPencilAlt } /> }
                   >
                     Editar
-                  </Button>
+                  </Button>)}
                 </Td>
               </Tr>
               <Tr>
-                <Td
-                  padding='6'
-                >
-                  <Checkbox
-                    colorScheme='green'
-                  />
+                <Td  paddingX={ ["4", "4", "6"] }>
+                  <Checkbox colorScheme="green" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text
-                      fontWeight='bold'
-                    >
-                      Carlos Henrique dos Santos
-                    </Text>
-                    <Text
-                      fontSize='sm'
-                      color='gray.300'
-                    >
+                    <Text fontWeight="bold">Carlos Henrique dos Santos</Text>
+                    <Text fontSize="sm" color="gray.300">
                       carlos.caulin@gmail.com
                     </Text>
                   </Box>
                 </Td>
+                { isWideVersion && <Td>10 de Julho, 2021</Td> }
                 <Td>
-                  10 de Julho, 2021
-                </Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
+                  { isWideVersion && (<Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
                     leftIcon={ <Icon as={ FaPencilAlt } /> }
                   >
                     Editar
-                  </Button>
+                  </Button>)}
                 </Td>
               </Tr>
               <Tr>
-                <Td
-                  padding='6'
-                >
-                  <Checkbox
-                    colorScheme='green'
-                  />
+                <Td  paddingX={ ["4", "4", "6"] }>
+                  <Checkbox colorScheme="green" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text
-                      fontWeight='bold'
-                    >
-                      Jean Fernandes de Macedo
-                    </Text>
-                    <Text
-                      fontSize='sm'
-                      color='gray.300'
-                    >
+                    <Text fontWeight="bold">Jean Fernandes de Macedo</Text>
+                    <Text fontSize="sm" color="gray.300">
                       jfmacedo91@gmail.com
                     </Text>
                   </Box>
                 </Td>
+                { isWideVersion && <Td>26 de Julho, 2021</Td> }
                 <Td>
-                  26 de Julho, 2021
-                </Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
+                  { isWideVersion && (<Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
                     leftIcon={ <Icon as={ FaPencilAlt } /> }
                   >
                     Editar
-                  </Button>
+                  </Button>)}
                 </Td>
               </Tr>
             </Tbody>
