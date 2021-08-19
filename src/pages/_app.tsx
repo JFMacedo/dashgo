00c8565e709f1931530c45075eb78b48a2df551a
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-08-19 06:56:18 -0300

    :wrench: miragejs configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
 
-import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from "../services/mirage"
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext"
 
-import { theme } from '../styles/theme'
+import { theme } from "../styles/theme"
+
+if(process.env.NODE_ENV === "development") {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
